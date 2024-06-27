5c3c8a015d39d473e86b9eefd7d3b9bba7b28644
root <root@DevServer.pivas.net>
2024-06-27 23:27:05 +0800

    1. add davPush.js and modify saveDockerImage workflow.

---
+++ davPush.js
@@ -0,0 +1,23 @@
+import {
+    createClient
+} from "webdav";
+import {
+    existsSync,
+    createReadStream
+} from "fs";
+import {
+    basename
+} from "path";
+
+function push(imageName) {
+    const fileName = imageName.replace(/[\/:]/g, "-");
+    const client = createClient(process.env.DAV_URL, {
+        username: process.env.DAV_USER,
+        password: process.env.DAV_PASS
+    });
+    let uploadStream = client.createWriteStream(`/SSD/dockerImg/${fileName}`);
+    createReadStream("image.tar").pipe(uploadStream);
+}
+
+console.log(process.argv);
+push(process.argv[2]);
