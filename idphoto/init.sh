1381b67251bdc7dfae17d4d691acb5667319db51
rkuk <rkuk@163.com>
2024-08-13 11:21:02 +0800

    1. fix typing mistake in idphoto docker script.

--- idphoto/init.sh
+++ idphoto/init.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Zeyi-Lin/HivisionIDPhotos
 mv HivisionIDPhotos/* ./ && rm -rf HivisionIDPhotos
 sed -i '/aliyun/d' Dockerfile
 sed -i 's/EXPOSE 8080/EXPOSE 8080 7860/' Dockerfile
-sed -i '/ENTRYPONIT/d' Dockerfile
+sed -i '/ENTRYPOINT/d' Dockerfile
 sed -i '$aENV GRADIO_SERVER_NAME="0.0.0.0"' Dockerfile
 sed -i '$aRUN chmod +x /app/entry.sh' Dockerfile
 sed -i '$aENTRYPOINT ["/app/entry.sh"]' Dockerfile
