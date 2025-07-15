d090d02facc0aecf5a5160045bbdea5060c60907
rkuk <rkuktemp@gmail.com>
2025-07-15 11:01:16 +0800

    Update codecombat init.sh to use new repo

--- codecombat/init.sh
+++ codecombat/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt update
 apt install -y git
-git clone https://github.com/Chaboi45/codecombat-premium.git 
-mv codecombat-premium/* ./
-rm -rf codecombat-premium init.sh
+git clone https://github.com/codecombat/codecombat.git
+mv codecombat/* ./
+rm -rf codecombat init.sh
